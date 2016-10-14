68dbab8a0266fbe261961d32a7e52759bd379f1c
user <user@FC09>
2016-10-14 10:04:56 +0800

    add do while

--- src/exercise/StringComperator.java
+++ src/exercise/StringComperator.java
@@ -1,5 +1,37 @@
 package exercise;
 
+import java.util.Scanner;
+
 public class StringComperator {
 
-}
+		
+		public static void main(String[]args){
+			
+			Scanner scanner=new Scanner(System.in);
+			int flag=0;
+			String one,two;
+			do{
+			System.out.println("輸入字串");
+			
+			one =scanner.next();
+			
+			System.out.println("輸入字串2");
+			
+			two = scanner.next();
+			
+			if(one.compareTo(two)==0){
+				
+				System.out.println("一樣");
+				flag =1;
+			}
+			else{
+				
+				System.out.println("不一樣");
+				
+				
+			}
+			}while(flag==0);
+		}
+
+	}
+
