7b52abee7d88b5aefdcfc81cdcaaf794ff5e83fe
user <user@FC09>
2016-10-14 09:53:16 +0800

    add Continue

---
+++ src/exercise/Continue.java
@@ -0,0 +1,11 @@
+package exercise;
+
+public class Continue {
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i ++) {
+			if(i%2 == 1 && i<5)
+		System.out.println(i);
+			continue;
+		}
+		}
+}
